47de3328756aa26d16868de82873052c5866f224
Sean Miller <sean@seanmiller.ca>
2014-02-12 00:28:29 -0500

    snippet: let-bang

--- snippets/ruby-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `ruby-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'ruby-mode
-		     '(("defs" "def self.$1($2)\n  $0\nend" "def.self" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Tue Feb 11 08:03:54 2014
