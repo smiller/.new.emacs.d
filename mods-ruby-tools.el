0349788825be4c47b767d0346d04c604a3f50e4f
Sean Miller <sean@seanmiller.ca>
2014-12-16 02:19:04 -0500

    Add senny-ruby-interpolate for # in ""

--- mods-ruby-tools.el
+++ mods-ruby-tools.el
@@ -3,3 +3,18 @@
 (global-set-key (kbd "C-c q :") 'ruby-tools-to-symbol)
 (global-set-key (kbd "C-c q '") 'ruby-tools-to-single-quote-string)
 (global-set-key (kbd "C-c q \"") 'ruby-tools-to-double-quote-string)
+
+; http://blog.senny.ch/blog/2012/10/06/emacs-tidbits-for-ruby-developers/
+(defun senny-ruby-interpolate ()
+  "In a double quoted string, interpolate."
+  (interactive)
+  (insert "#")
+  (when (and
+         (looking-back "\".*")
+         (looking-at ".*\""))
+    (insert "{}")
+    (backward-char 1)))
+
+(eval-after-load 'enh-ruby-mode
+  '(progn
+     (define-key enh-ruby-mode-map (kbd "#") 'senny-ruby-interpolate)))
