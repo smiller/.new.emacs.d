60099d0954e4b59fb18a1827fa88a3f2fbdfcec7
Sean Miller <sean@seanmiller.ca>
2015-09-10 22:02:52 -0400

    Add org-capture-templates and emergency colours
    (Emergency colours?  For some reason my laptop lost 256 colours,
    even though iTerm was set as xterm-256color.  Adding explicit
    colours brought them back.  There's probably a more elegant solution.)

--- init.el
+++ init.el
@@ -14,14 +14,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background nil))))
+ '(default ((t (:inherit nil :stipple nil :background "#FDF5E6" :foreground "grey22" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
  '(bmkp-gnus ((t (:foreground "green"))))
  '(bmkp-heading ((t (:foreground "green"))))
  '(bmkp-local-directory ((t (:background "black" :foreground "green"))))
  '(bmkp-local-file-with-region ((t (:foreground "green"))))
  '(bmkp-local-file-without-region ((t (:foreground "green"))))
+ '(bold ((t (:weight ultra-bold))))
  '(helm-selection ((t (:background "green" :underline nil))))
- '(italic ((t (:foreground "color-30" :slant oblique)))))
+ '(italic ((t (:foreground "mediumseagreen" :slant oblique))))
+ '(org-link ((t (:foreground "purple3" :underline t)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
